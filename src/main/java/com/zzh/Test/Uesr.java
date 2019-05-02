65083b083cef6bcc5c4e24a5b0f2a8e807fb02c9
zzh <513985092@qq.com>
2019-05-02 16:05:01 +0800

    TestGit

---
+++ src/main/java/com/zzh/Test/Uesr.java
@@ -0,0 +1,12 @@
+package com.zzh.Test;
+
+/**
+ * Created with IntelliJ IDEA
+ * Created By 钟子豪
+ * Date: 2019/5/2
+ * Time: 16:01
+ */
+public class Uesr {
+    private  String name;
+    private int age;
+}
